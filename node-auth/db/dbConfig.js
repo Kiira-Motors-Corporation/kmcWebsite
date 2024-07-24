a3256e801673bc3d002cb4304515e58438e21a12
zaharah <nakawunguz19@gmail.com>
2024-07-24 11:58:35 +0300

    Adding dynamic env variables to db

--- node-auth/db/dbConfig.js
+++ node-auth/db/dbConfig.js
@@ -1,11 +1,13 @@
+require("dotenv").config();
+
 const mysql = require("mysql");
 
 // Create MySQL connection
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "admin",
-    database: "kmc",
+    host: process.env.DB_HOSTNAME,
+    user: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
   });
 
   db.connect((err) => {
--- node-auth/models/index.js
+++ node-auth/models/index.js
@@ -1,21 +1,23 @@
 'use strict';
-
+require("dotenv").config();
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+// const env = process.env.NODE_ENV || 'development';
+// const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
 
+
 // let sequelize= new Sequelize("mysql://root:admin@localhost:3306/kmc",{dialect:"mysql"})
-let sequelize;
-if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+let sequelize = new Sequelize(process.env.DB_NAME,process.env.DB_USERNAME,process.env.DB_PASSWORD,{host:process.env.DB_HOSTNAME,dialect:"mysql"})
+
+// if (config.use_env_variable) {
+//   sequelize = new Sequelize(process.env[config.use_env_variable], config);
+// } else {
+//   sequelize = new Sequelize(config.database, config.username, config.password, config);
+// }
 
 fs
   .readdirSync(__dirname)
